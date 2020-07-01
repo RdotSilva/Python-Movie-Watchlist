8798620bb6eec0bc5e9e5b431b4af44d3127aed2
RdotSilva <RyandotSilva@gmail.com>
2020-06-30 20:16:28 -0400

    Update CREATE_MOVIES_TABLE query

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ CREATE_MOVIES_TABLE = """CREATE TABLE IF NOT EXISTS movies (
     title TEXT,
     release_timestamp REAL,
     watched INTEGER
-); """
+);"""
 
 INSERT_MOVIE = (
     "INSERT INTO movies (title, release_timestamp, watched) VALUES (?, ?, 0);"
