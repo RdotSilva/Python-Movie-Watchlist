939fcd9de1f1f528efae31db41100169435ff047
RdotSilva <RyandotSilva@gmail.com>
2020-07-02 20:46:09 -0400

    Update queries to handle watched movies

--- database.py
+++ database.py
@@ -3,19 +3,16 @@ import sqlite3
 
 CREATE_MOVIES_TABLE = """CREATE TABLE IF NOT EXISTS movies (
     title TEXT,
-    release_timestamp REAL,
-    watched INTEGER
+    release_timestamp REAL
 );"""
 
-INSERT_MOVIE = (
-    "INSERT INTO movies (title, release_timestamp, watched) VALUES (?, ?, 0);"
-)
+INSERT_MOVIE = "INSERT INTO movies (title, release_timestamp) VALUES (?, ?, 0);"
 
 SELECT_ALL_MOVIES = "SELECT * FROM movies;"
 
 SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > ?;"
 
-SELECT_WATCHED_MOVIES = "SELECT * FROM movies WHERE watched = 1;"
+SELECT_WATCHED_MOVIES = "SELECT * FROM watched WHERE watcher_name = ?;"
 
 SET_MOVIE_WATCHED = "UPDATE movies set watched = 1 WHERE title = ?;"
 
