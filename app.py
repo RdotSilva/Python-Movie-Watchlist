a2673a132de80107f1c67b01ba6f61edfe574981
RdotSilva <RyandotSilva@gmail.com>
2020-07-03 20:19:45 -0400

    Add print_watched_movie_list method

--- app.py
+++ app.py
@@ -41,6 +41,13 @@ def print_movie_list(heading, movies):
     print("--- \n")
 
 
+def print_watched_movie_list(username, movies):
+    print(f"-- {username}'s watched movies --")
+    for movie in movies:
+        print(f"{movie[1]}")
+    print("---- \n")
+
+
 def prompt_watch_movie():
     """
     Mark a movie as watched
@@ -70,6 +77,6 @@ while user_input != "6":
     elif user_input == "5":
         username = input("Username: ")
         movies = database.get_watched_movies(username)
-        print_movie_list("Watched", movies)
+        print_watched_movie_list(username, movies)
     else:
         print("Invalid input, please try again!")
