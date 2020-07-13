2a169caf1d2d939a4f837b6ae82cf977be2ba708
RdotSilva <RyandotSilva@gmail.com>
2020-07-12 22:29:02 -0400

    Refactor prompt_add_movie method

--- app.py
+++ app.py
@@ -40,12 +40,12 @@ def prompt_add_movie():
     """
     Prompt a user to add new movie
     """
-    title = input("Movie title: ")
+    name = input("Movie title: ")
     release_date = input("Release date (dd-mm-YYYY): ")
     parsed_date = datetime.datetime.strptime(release_date, "%d-%m-%Y")
     timestamp = parsed_date.timestamp()
 
-    database.add_movie(title, timestamp)
+    database.add_movie(name, timestamp)
 
 
 def print_movie_list(heading, movies):
