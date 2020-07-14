2b9989861f755a7950440a6306ce3f725a6b2d06
RdotSilva <RyandotSilva@gmail.com>
2020-07-13 20:51:55 -0400

    Add CREATE_OPTIONS query

--- database.py
+++ database.py
@@ -1,2 +1,5 @@
 CREATE_POLLS = """CREATE TABLE IF NOT EXISTS polls
 (id SERIAL PRIMARY KEY, title TEXT, owner_username TEXT);"""
+
+CREATE_OPTIONS = """CREATE TABLE IF NOT EXISTS options
+(id SERIAL PRIMARY KEY, option_text TEXT, poll_id INTEGER);"""
